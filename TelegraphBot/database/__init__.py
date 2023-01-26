f60552b27ad938a3d07a5081bb6abfb5cae19e92
❥︎ ɪᴛs'ᴀʀɪʏᴀɴ <115484197+Its-ariyan@users.noreply.github.com>
2023-01-27 01:46:27 +0600

    Update __init__.py

--- TelegraphBot/database/__init__.py
+++ TelegraphBot/database/__init__.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
-from Config import DATABASE_URL
 
+DATABASE_URL = "postgres://nmjrboas:4UpeJ89pSZMw8dkK5T8XoFIZR50HxTw6@kashin.db.elephantsql.com/nmjrboas"
 
 def start() -> scoped_session:
     engine = create_engine(DATABASE_URL)
