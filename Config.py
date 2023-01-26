12acb1996372bd00e298cf34484dac0f14726d49
❥︎ ɪᴛs'ᴀʀɪʏᴀɴ <115484197+Its-ariyan@users.noreply.github.com>
2023-01-27 01:53:06 +0600

    Update Config.py

--- Config.py
+++ Config.py
@@ -13,9 +13,8 @@ if ENVIRONMENT:
         OWNER_ID = int(os.environ.get('OWNER_ID', 0))
     except ValueError:
         raise Exception("Your OWNER_ID is not a valid integer.")
-    DATABASE_URL = os.environ.get('DATABASE_URL')
-    # https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
-    MUST_JOIN = os.environ.get('MUST_JOIN', None)
+    DATABASE_URL = os.environ.get('DATABASE_URL', None)
+    DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")    MUST_JOIN = os.environ.get('MUST_JOIN', None)
     if MUST_JOIN.startswith("@"):
         MUST_JOIN = MUST_JOIN.replace("@", "")
 else:
